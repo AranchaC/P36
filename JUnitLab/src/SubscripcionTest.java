3509859e8d71f2fb808edf779c843032737fbb64
AranchaC <aranchachicharro@gmail.com>
2023-02-10 10:01:30 +0100

    test 2

--- JUnitLab/src/SubscripcionTest.java
+++ JUnitLab/src/SubscripcionTest.java
@@ -6,7 +6,7 @@ class SubscripcionTest {
 
 	@Test
 	public void testPrecioPorMes() {
-		double esperado=1;
+		double esperado=100;
 		Subscripcion s = new Subscripcion(200,2) ;
 		double resultado = s.precioPorMes();
 		assertEquals(esperado, resultado,0) ;
@@ -14,7 +14,7 @@ class SubscripcionTest {
 	
 	@Test
 	public void testprecioPorMes2() {
-		double esperado=67;
+		double esperado=67.66;
 		Subscripcion s = new Subscripcion(200,3) ;
 		double resultado= s.precioPorMes();
 		assertEquals(esperado, resultado, 0) ;
